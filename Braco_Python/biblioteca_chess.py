38b80ac1084136298441e0b6a7cb5c8a45eabe4f
lucas-lanes <lucas.lanes@engenharia.ufjf.br>
2023-08-20 22:54:28 -0300

    funções novas

--- Braco_Python/biblioteca_chess.py
+++ Braco_Python/biblioteca_chess.py
@@ -1,5 +1,6 @@
 import chess
 import chess.engine
+from stockfish import Stockfish
 
 # Seleciona a engine desejada e especifica o pathing de onde ela foi baixada
 engine = chess.engine.SimpleEngine.popen_uci(r"D:\Arquivos\BracoRobotico2023\Braco_Python\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe") 
@@ -7,9 +8,9 @@ engine = chess.engine.SimpleEngine.popen_uci(r"D:\Arquivos\BracoRobotico2023\Bra
 # Inicializa o tabuleiro
 board = chess.Board()
 
-"""
-# ALGUNS CONCEITOS BÁSICOS PARA EU EXPLICAR PRA PRINCESA DA LAIS
 
+# ALGUNS CONCEITOS BÁSICOS PARA EU EXPLICAR PRA PRINCESA DA LAIS
+"""
 # Pega a jogada     
 jogada_usuario = input("Qual foi o movimento feito pelo usuário? ")
     
@@ -20,9 +21,15 @@ move1 = chess.Move.from_uci(jogada_usuario)
 board.push(move1) 
 """
 
-"""
-# COMO VERIFICAR SE O MOVIMENTO FOI LEGAL OU NÃO (TRUE OU FALSE)
 
+# DIFICULDADE
+"""
+engine.configure({"Skill Level": 20})
+"""
+
+
+# COMO VERIFICAR SE O MOVIMENTO FOI LEGAL OU NÃO (TRUE OU FALSE)
+"""
 jogada_usuario = input("Qual foi o movimento feito pelo usuário?")
 mov = chess.Move.from_uci(jogada_usuario)
 
@@ -30,3 +37,50 @@ while board.is_legal(mov) == False:
         jogada_usuario = input("Movimento inválido, digite novamente: ")
         mov = chess.Move.from_uci(jogada_usuario)
 """
+
+
+"""
+stockfish=Stockfish(r"D:\Arquivos\BracoRobotico2023\Braco_Python\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe")
+stockfish.set_depth(20)#How deep the AI looks
+stockfish.set_skill_level(20)#Highest rank stockfish
+stockfish.get_parameters()
+"""
+
+
+# PARTIDA
+"""
+while True:
+        movb = engine.play(board, chess.engine.Limit(time=1))
+        print(movb.move)
+        
+        if board.is_castling(movb.move) == True:
+                input("Vai rolar o roque")
+                
+        if board.is_kingside_castling(movb.move) == True:
+                input("Vai rolar o roque na ala do Rei")
+                
+        if board.is_queenside_castling(movb.move) == True:
+                input("Vai rolar o roque na ala da Rainha")
+                
+        board.push(movb.move)
+        # input("Pressione Enter para prosseguir")
+        print(board)
+        
+        
+        movp = engine.play(board, chess.engine.Limit(time=1))
+        
+        print(movp.move)
+        if board.is_castling(movp.move) == True:
+                input("Vai rolar o roque")
+                
+        if board.is_kingside_castling(movp.move) == True:
+                input("Vai rolar o roque na ala do Rei")
+                
+        if board.is_queenside_castling(movp.move) == True:
+                input("Vai rolar o roque na ala da Rainha")
+                
+        board.push(movp.move)
+        # input("Pressione Enter para prosseguir")
+        print(board)
+"""
+
